7a9c0ae7c19e9ea5c6ffc8bd4b7448cac5138cd2
Vitalii2005 <vitalikqazzaq4@gmail.com>
2024-09-30 16:38:24 +0300

    Додано код для варіанта 5

--- var.5/lab5.cpp
+++ var.5/lab5.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Прізвище: Кец" << endl;
+    cout << "Ім'я: Віталій" << endl;
+    cout << "Номер групи: KI-412" << endl;
+    return 0;
+}
 
